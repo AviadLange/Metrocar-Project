03867e7ae6340411f0f6f50cd6fd6a6109afd817
Aviad <134489612+AviadLange@users.noreply.github.com>
2023-10-25 14:13:47 +0300

    Update Rides by Hour.sql

--- SQL/Rides by Hour.sql
+++ SQL/Rides by Hour.sql
@@ -1,7 +1,9 @@
 --This query groups the rides' data by the hour of the day.
 SELECT TO_CHAR(request_ts, 'HH24') AS request_time,
 	TO_CHAR(request_ts, 'DD/MM/YYYY') AS request_day,
-	COUNT(ride_id) AS number_of_rides, platform, age_range
+	COUNT(ride_id) AS number_of_rides,
+	platform,
+	age_range
 FROM signups AS s
 JOIN app_downloads AS a
 ON a.app_download_key = s.session_id
